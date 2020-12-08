4c7b3a529803531448f5085310232c1a9e07392e
wfedev <wfedev@gmail.com>
2020-12-08 20:35:07 +0900

    cloud edit

--- ui/4-Cloud/script.js
+++ ui/4-Cloud/script.js
@@ -1,5 +1,56 @@
-let text = document.getElementById('text');
-window.addEventListener('scroll', function() {
+// 1-스크롤 상관없이 애니메이션 바로 실행
+// let text = document.getElementById('text');
+// window.addEventListener('scroll', function() {
+//   let value = window.scrollY;
+//   text.style.marginBottom = value * 1 + 'px';
+// });
+
+
+// 2-스크롤 내렸다가 올리면 애니메이션이 다시 실행
+let text = document.getElementById("text");
+
+// let images = document.querySelectorAll(".clouds > img");
+
+// function imageAnimate(scrollY) {
+//   let bannerHeight = document.querySelector(".top").clientHeight;
+//   images.forEach( (item) => {
+//     if (scrollY >= bannerHeight) {
+//       if (!item.classList.contains("pause")) {
+//         item.classList.add("pause")
+//       }
+//     } else {
+//       if (item.classList.contains("pause")) {
+//         item.classList.remove("pause")
+//       }
+//     }
+//   })
+// }
+
+let imageWrapper = document.querySelector(".clouds");
+
+function imageAnimate(scrollY) {
+  let bannerHeight = document.querySelector(".top").clientHeight;
+
+    if (scrollY >= bannerHeight) {
+      if (!imageWrapper.classList.contains("pause")) {
+        imageWrapper.classList.add("pause")
+      }
+    } else {
+      if (imageWrapper.classList.contains("pause")) {
+        imageWrapper.classList.remove("pause")
+      }
+    }
+}
+
+window.addEventListener("load", function() {
   let value = window.scrollY;
-  text.style.marginBottom = value * 1 + 'px';
-});
+  imageAnimate(value)
+})
+
+window.addEventListener("scroll", function() {
+  let value = window.scrollY;
+
+  imageAnimate(value);
+
+  text.style.marginBottom = value * 2 + 'px';
+})
