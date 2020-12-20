2720ab3a30d2f891e3b47485b0a0de875acf39d7
wfedev <wfedev@gmail.com>
2020-12-21 06:13:01 +0900

    ㄴ

--- ui/3-1-Salad/script.js
+++ ui/3-1-Salad/script.js
@@ -1,14 +1,20 @@
+/*
+Salad slide 만드는 방법
+https://diary-blog.github.io/salad-carousel-slide/
+*/
+
 var swiper = new Swiper('.swiper-container', {
   effect: 'coverflow',
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: 'auto',
+
   coverflowEffect: {
     rotate: 0,
     stretch: 0,
     depth: 0,
     modifier: 1,
-    slideShadows: true,
+    slideShadows: false,
   },
   loop: true,
 });
