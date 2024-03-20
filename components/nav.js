c5b75f7d058eecc2f1523a49dba3eb859aaf5c31
wfedev <wfedev@gmail.com>
2024-03-20 11:55:50 +0900

    Simplify menu creation and event handling for enhanced clarity

--- components/nav.js
+++ components/nav.js
@@ -2,18 +2,14 @@ import { updateLinkHref, highlightActiveLink } from './path.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   const menu = createMenu();
-  const firstElement = document.body.firstChild;
-  document.body.insertBefore(menu, firstElement);
+  document.body.insertBefore(menu, document.body.firstChild);
   if (sessionStorage.getItem('mode') === 'dark') {
     document.body.classList.add('dark');
-    const modeButton = document.getElementById('modeButton');
-    modeButton.textContent = '🌙';
+    document.getElementById('modeButton').textContent = '🌙';
   }
-  globalThis.onload = function() {
-    const menu = document.getElementById('menu');
-    const checkbox = document.querySelector('#menuToggle input');
-    menu.addEventListener('click', function() {
-      checkbox.checked = false;
+  globalThis.onload = () => {
+    document.getElementById('menu').addEventListener('click', () => {
+      document.querySelector('#menuToggle input').checked = false;
     });
   }
   updateModeButtonText();
@@ -21,74 +17,53 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const createMenu = () => {
   const originalHrefs = [];
-  const buildContent = () => {
-    const newContent = document.createElement('nav');
-    newContent.innerHTML = `
-      <div id="menuToggle">
-        <input type="checkbox" />
-        <span></span>
-        <span></span>
-        <span></span>
-        <ul id="menu">
-          <li><a href="/">Home</a></li>
-          <li><a href="../pages/1/index.html" class="code">CSS</a></li>
-          <li><a href="../pages/2/index.html" class="code">WebGL</a></li>
-          <li><a href="../pages/3/index.html" class="drawing">GenAI</a></li>
-          <li><a href="../pages/4/index.html" class="drawing">iPad</a></li>
-          <li><a href="../pages/5/index.html">etc</a></li>
-          <li></li>
-        </ul>
-      </div>
-      `;
-    return newContent;
-  };
+  const newContent = document.createElement('nav');
+  newContent.innerHTML = `
+    <div id="menuToggle">
+      <input type="checkbox" />
+      <span></span>
+      <span></span>
+      <span></span>
+      <ul id="menu">
+        <li><a href="/">Home</a></li>
+        <li><a href="../pages/1/index.html" class="code">CSS</a></li>
+        <li><a href="../pages/2/index.html" class="code">WebGL</a></li>
+        <li><a href="../pages/3/index.html" class="drawing">GenAI</a></li>
+        <li><a href="../pages/4/index.html" class="drawing">iPad</a></li>
+        <li><a href="../pages/5/index.html">etc</a></li>
+      </ul>
+    </div>
+  `;
 
-  const mode = () => {
+  newContent.querySelectorAll('a').forEach((link, index) => {
+    originalHrefs[index] = link.getAttribute('href');
+    updateLinkHref(link, originalHrefs[index], globalThis.location);
+  });
+  highlightActiveLink(newContent.querySelectorAll('a'), globalThis.location.pathname);
+
+  document.addEventListener("DOMContentLoaded", () => {
+    newContent.querySelectorAll('.main-card li').forEach((item, index) => {
+      if (index !== 0) {
+        item.style.setProperty('--index', `"${index}"`);
+      }
+    });
+  });
+
+  globalThis.addEventListener('popstate', () => {
+    highlightActiveLink(newContent.querySelectorAll('a'), globalThis.location.pathname);
+  });
+
+  globalThis.mode = () => {
     const body = document.body;
     body.classList.toggle('dark');
-    const isDarkMode = body.classList.contains('dark');
-    sessionStorage.setItem('mode', isDarkMode ? 'dark' : 'light');
+    sessionStorage.setItem('mode', body.classList.contains('dark') ? 'dark' : 'light');
     updateModeButtonText();
   };
-  const updateModeButtonText = () => {
-    const modeButton = document.getElementById('modeButton');
+
+  globalThis.updateModeButtonText = () => {
     const modeText = sessionStorage.getItem('mode') === 'dark' ? '🌼' : '🌙';
-    modeButton.textContent = modeText;
+    document.getElementById('modeButton').textContent = modeText;
   };
-
-  globalThis.mode = mode;
-  globalThis.updateModeButtonText = updateModeButtonText;
-  
-  const initializeLinks = (newContent) => {
-    const links = newContent.querySelectorAll('a');
-    links.forEach((link, index) => {
-      originalHrefs[index] = link.getAttribute('href');
-      updateLinkHref(link, originalHrefs[index], globalThis.location);
-    });
-    highlightActiveLink(links, globalThis.location.pathname);
-  };
-
-  const addEventListeners = (newContent) => {
-    document.addEventListener("DOMContentLoaded", () => {
-      const items = newContent.querySelectorAll('.main-card li');
-      items.forEach((item, index) => {
-        if (index !== 0) {
-          const indexText = index;
-          item.style.setProperty('--index', `"${indexText}"`);
-        }
-      });
-    });
-
-    globalThis.addEventListener('popstate', () => {
-      highlightActiveLink(newContent.querySelectorAll('a'), globalThis.location.pathname);
-    });
-  };
-
-  const newContent = buildContent();
-  initializeLinks(newContent);
-  addEventListeners(newContent);
 
   return newContent;
 };
-
-
